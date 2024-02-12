20f697c53295b6adfa56f6f06e19373d5fed4866
vivien <vivien.di.battista@gmail.com>
2024-02-12 16:23:14 +0100

    feat(config): init project

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,17 +1,64 @@
-/**
- * plugins/index.js
- *
- * Automatically included in `./src/main.js`
- */
-
 // Plugins
-import { loadFonts } from './webfontloader'
-import vuetify from './vuetify'
-import router from '../router'
+import { loadFonts } from "./webfontloader";
+import vuetify from "./vuetify";
+import router from "../router";
+import axios from "axios";
+import VueAxios from "vue-axios";
+// import snackbar from "./snackbar";
 
-export function registerPlugins (app) {
-  loadFonts()
-  app
-    .use(vuetify)
-    .use(router)
+export function registerPlugins(app) {
+  const isDev = process.env.NODE_ENV !== "production";
+
+  let BASE_URL_API = import.meta.env.VITE_BASE_URL_API;
+  if (isDev) {
+    BASE_URL_API = import.meta.env.VITE_BASE_URL_API_SERVE;
+  }
+
+  axios.interceptors.request.use((config) => {
+    return {
+      ...config,
+      baseURL: BASE_URL_API,
+      withCredentials: true,
+      validateStatus: function (status) {
+        return status >= 200 && status < 300;
+      },
+    };
+  });
+
+  axios.interceptors.response.use(
+    (response) => {
+      return Promise.resolve(response);
+    },
+    (error) => {
+      let errorsContent = [];
+      if (error.response) {
+        let status = error.response.status;
+        let message =
+          status +
+          " : " +
+          (error.response.data.phrase ?? " : " + error.message);
+        errorsContent.push(message);
+        let errors = error.response.data.errors;
+        if (errors !== null) {
+          Object.keys(errors).forEach(function (prop) {
+            errorsContent.push(errors[prop][0]);
+          });
+        }
+      } else {
+        errorsContent.push(error.message ?? "Undefined");
+      }
+    //   let snackbar = app.config.globalProperties.$snackbar;
+    //   if (!axios.isCancel(error)) {
+    //     snackbar.error(errorsContent);
+    //   }
+      return Promise.reject(error.response);
+    }
+  );
+
+  window.axios = axios;
+  loadFonts();
+
+//   app.use(vuetify).use(router).use(VueAxios, axios).use(snackbar, {
+//     timeout: 3500,
+//   });
 }
--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,58 @@
 // Plugins
-import vue from '@vitejs/plugin-vue'
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import vue from "@vitejs/plugin-vue";
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import ViteFonts from "unplugin-fonts/vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // Utilities
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from "vite";
+import { fileURLToPath, URL } from "node:url";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const baseConfig = {
   plugins: [
-    vue({ 
-      template: { transformAssetUrls }
+    vue({
+      template: { transformAssetUrls },
     }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
       autoImport: true,
       styles: {
-        configFile: 'src/styles/settings.scss',
+        configFile: "src/styles/settings.scss",
+      },
+    }),
+    basicSsl(),
+    ViteFonts({
+      google: {
+        families: [
+          {
+            name: "Roboto",
+            styles: "wght@100;300;400;500;700;900",
+          },
+        ],
       },
     }),
   ],
-  define: { 'process.env': {} },
+  define: { "process.env": {} },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ],
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
-  server: {
-    port: 3000,
-  },
-})
+};
+
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  if (command === "serve") {
+    return {
+      ...baseConfig,
+      base: env.VITE_BASE_URL_APP,
+      server: {
+        https: true,
+        host: "127.0.0.1",
+        port: 8080,
+      },
+    };
+  } else {
+    return { ...baseConfig, base: env.VITE_BASE_URL_APP };
+  }
+});
