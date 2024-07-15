a453b29a04982b0b8a35c8b6220767dc4a3203a5
vivien <vivien.di.battista@gmail.com>
2024-07-15 14:55:59 +0200

    feat(help_page): page et menus

--- src/components/TipList.vue
+++ src/components/TipList.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container class="fill-height">
-    <v-responsive class="d-flex align-center text-center fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <h1 class="text-h2 font-weight-bold">Welcome &#x1F42D;</h1>
     </v-responsive>
   </v-container>
 </template>
--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-app-bar color="#CC0000" flat>
+  <v-app-bar color="light-blue-darken-2">
     <v-app-bar-title align="center" class="text-h5 font-weight-bold">
-      <v-icon icon="mdi-note-multiple-outline" class="mr-1" />
+      <v-icon icon="mdi-rodent" class="mr-1" />
       Traveling Rat
     </v-app-bar-title>
   </v-app-bar>
